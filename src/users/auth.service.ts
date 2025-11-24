ff87991dd7771955f9d62abf69cdaa991b40c2c6
EoLima <lucasl.ima@icloud.com>
2025-11-24 18:44:23 -0300

    feat: singIn

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -29,11 +33,15 @@ export class AuthService {
   async signIn(email: string, password: string) {
     const [user] = await this.usersService.find(email);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
 
-    if (user.password !== password) {
-      throw new Error('Bad password');
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Bad password');
     }
 
     return user;
