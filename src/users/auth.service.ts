ac8dd225c31943cb63604fa6554b3671754ee99d
EoLima <lucasl.ima@icloud.com>
2025-11-24 18:33:39 -0300

    feat: authentication from scratch and singup

---
+++ src/users/auth.service.ts
@@ -0,0 +1,41 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signUp(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in use');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const saltedHash = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, saltedHash);
+
+    return user;
+  }
+
+  async signIn(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (user.password !== password) {
+      throw new Error('Bad password');
+    }
+
+    return user;
+  }
+}
