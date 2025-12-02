c8e3cbaa833f3be4ca789fe16b1426c434a5296a
EoLima <lucasl.ima@icloud.com>
2025-12-01 23:04:07 -0300

    feat: globally scoped interceptors

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,7 +9,14 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
   imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
